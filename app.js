1b7c893433cdf89fe33a1b8f968ff3c43490d27f
AlexeiVartoumian <55895355+AlexeiVartoumian@users.noreply.github.com>
2020-06-21 20:08:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let tally=0
 const answers = ['true','false','true']
 
@@ -17,11 +16,6 @@ let q3Answer;
 console.log(q1)
 console.log(q2)
 console.log(q3)
-
-function clearTally(){
-    tally = 0
-}
-
 
 btn.addEventListener('click', function(){
     
@@ -66,6 +60,9 @@ btn.addEventListener('click', function(){
  clearTally()
 })
 
+function clearTally(){
+    tally = 0
+}
 
 /*
 for(var i = 0; i <q1.length; i++){
@@ -88,3 +85,133 @@ if(document.getElementById('option-1').checked){
 }
 
 */
+function isTouching(a, b) {
+	const aRect = a.getBoundingClientRect();
+	const bRect = b.getBoundingClientRect();
+	
+	return !(
+		aRect.top + aRect.height < bRect.top ||
+		aRect.top > bRect.top + bRect.height ||
+		aRect.left + aRect.width < bRect.left ||
+		aRect.left > bRect.left + bRect.width 
+		
+	);
+}
+
+const avatar =document.querySelector('#player');
+const coin = document.querySelector('#coin');
+const coin2 = document.querySelector('.coin');
+
+let score = 0;
+
+window.addEventListener('keydown', function(e){
+	if(e.key ==='ArrowDown' || e.key ==='Down'){
+	moveVertical(avatar, 50)
+	
+} else if( e.key === 'ArrowUp' || e.key ==='up'){
+	moveVertical(avatar, -50)
+	
+} else if( e.key === 'ArrowRight' || e.key ==='Right'){
+	moveHorizontal(avatar, 50)
+	avatar.style.transform = 'scale(1,1)'
+	
+}else if( e.key === 'ArrowLeft' || e.key ==='Left'){
+	moveHorizontal(avatar,-50)
+	avatar.style.transform= 'scale(-1,1)'
+}
+if(isTouching(avatar, coin)){ moveCoin()
+score++
+h3.innerHTML= `YOUR CURRENT SCORE IS ${score}`;
+}
+if(isTouching(avatar, coin2)){ moveCoin2()
+	score++
+	h3.innerHTML= `YOUR CURRENT SCORE IS ${score}`;
+	}
+})
+
+const moveVertical = (element,amount) => {
+	const currentTop= extractPosition(element.style.top);
+	element.style.top = `${currentTop + amount}px`
+}
+const moveHorizontal = (element,amount) => {
+	const currentLeft= extractPosition(element.style.left);
+	element.style.left = `${currentLeft + amount}px`
+}
+
+
+const extractPosition = (pos) => {
+	if(!pos) return 100;
+	//turn string into number
+	return parseInt(pos.slice(0,-2))
+}
+
+const moveCoin = () => {
+	const y = Math.floor(Math.random() * window.innerHeight)
+	const x = Math.floor(Math.random() * window.innerWidth)
+	coin.style.top = `${y}px`
+	coin.style.left = `${x}px`
+	
+}
+const moveCoin2 = () => {
+	const y = Math.floor(Math.random() * window.innerHeight)
+	const x = Math.floor(Math.random() * window.innerWidth)
+	coin2.style.top = `${y}px`
+	coin2.style.left = `${x}px`
+	
+}
+
+
+moveCoin()
+
+
+
+const h3 = document.querySelector('.score-tracker')
+h3.innerHTML += `YOUR CURRENT SCORE IS ${score}`
+
+
+function playAudio(){
+	music.play()
+	var theCount = 60 * 1.36666666666,
+	display = document.querySelector('#time');
+	startTimer(theCount, display);
+	resetTimeout()
+	
+}
+
+
+function startTimer(duration,display){
+	var timer = duration,minutes,seconds;
+	setInterval(function(){
+		minutes = parseInt(timer/60,10);
+		seconds = parseInt(timer % 60,10);
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+		display.textContent = minutes + ":" + seconds;
+
+		if(--timer <0){
+			timer = duration
+			gameOver()
+		}
+		
+	}, 1000)
+	
+}
+let timeOut = setTimeout("location.reload(true);",82000)
+
+function resetTimeout(){
+	clearTimeout(timeOut);
+	timeOut=setTimeout("location.reload(true);",82000)
+	avatar.style.position = "absolute"
+	avatar.style.top="100px"
+	avatar.style.left = "100px"
+	
+}
+
+function gameOver(){
+	alert(h3.innerHTML=`Game over! your final score is ${score}`)
+}
+
+const music = document.querySelector('#music')
+
+
+music.addEventListener('focus',function(){this.blur();},false);
